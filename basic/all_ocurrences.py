55d5f710fdad459ff4bcc3db46fe990da9189197
Murilo Zimerman Fortaleza <mzimermanfortaleza@gmail.com>
2025-10-24 12:12:08 -0300

    Implemented all Vale ocurrences

--- basic/all_ocurrences.py
+++ basic/all_ocurrences.py
@@ -1,1 +1,14 @@
+from pyspark import SparkConf, SparkContext
+import os
+import shutil
+
+config = SparkConf().setAppName("allOcurrencesVale").setMaster("local")
+sparkContext = SparkContext.getOrCreate(conf = config)
+rdd = sparkContext.textFile("../stock_details_5_years.csv")
+rddF = rdd.filter(lambda line : not "CLOSE" in line)
+rddMap = rddF.map(lambda line : line.split(",")[3])
+
+def montaTupla(line):
+	columns = line.split(",")
+	model : str = columns[8]
 
