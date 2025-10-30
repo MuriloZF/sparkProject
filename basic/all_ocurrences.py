c22d7dc3e5d51cf3988dbda1dbd2ac314eb93c97
Murilo Zimerman Fortaleza <mzimermanfortaleza@gmail.com>
2025-10-30 14:22:50 -0300

    Implemented all_ocurrences(show all VALE's ocurrences)

--- basic/all_ocurrences.py
+++ basic/all_ocurrences.py
@@ -5,10 +5,14 @@ import shutil
 config = SparkConf().setAppName("allOcurrencesVale").setMaster("local")
 sparkContext = SparkContext.getOrCreate(conf = config)
 rdd = sparkContext.textFile("../stock_details_5_years.csv")
-rddF = rdd.filter(lambda line : not "CLOSE" in line)
-rddMap = rddF.map(lambda line : line.split(",")[3])
+rddF = rdd.filter(lambda line : not "Close" in line)
+rdd_cols = rddF.map(lambda line : line.split(","))
+rdd_vale = rdd_cols.filter(lambda cols : cols[8].upper() == "VALE").map(lambda cols : ",".join(cols))
+def removeDir(directory):
+	if os.path.exists(directory):
+		shutil.rmtree(directory)
 
-def montaTupla(line):
-	columns = line.split(",")
-	model : str = columns[8]
+directory :str = "outputAllVale"
 
+removeDir(directory)
+rdd_vale.saveAsTextFile(directory)
